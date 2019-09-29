217061b4fcc64918993af90f2c2359286a64aab1
lynkeib <liuchengyin1234@126.com>
2019-09-29 10:55:28 -0700

    /

--- backend/apis/admin.py
+++ backend/apis/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import App
 
 # Register your models here.
+# class AppAdmin(admin.ModelAdmin):
+#     list_display = ['appid',
+#                     'category',
+#                     'application',
+#                     'name',
+#                     'publish_date',
+#                     'url',
+#                     'desc']
+
+
+# admin.site.register(App, AppAdmin)
+admin.site.register(App)
--- backend/authorization/admin.py
+++ backend/authorization/admin.py
@@ -3,8 +3,9 @@ from .models import User
 
 
 # Register your models here.
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['open_id', 'nickname', 'focus_cities', 'focus_constellations', 'focus_stocks']
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ['open_id', 'nickname', 'focus_cities', 'focus_constellations', 'focus_stocks']
 
 
-admin.site.register(User, UserAdmin)
+# admin.site.register(User, UserAdmin)
+admin.site.register(User)
