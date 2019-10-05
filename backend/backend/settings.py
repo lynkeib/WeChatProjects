e6a5f2d975dae02285c0c10280a3f8108a310580
lynkeib <liuchengyin1234@126.com>
2019-10-05 10:32:34 -0700

    .

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'backend',
         "USER": "root",
-        "PASSWORD": "",
+        "PASSWORD": "123456",
         "HOST": "127.0.0.1",
         "PORT": "3306"
     }
