d77eac87335dd4cd5ce64aeb7c85fa7bb981d9d6
lynkeib <liuchengyin1234@126.com>
2019-10-07 11:56:01 -0700

    Update test.py

--- backend/test.py
+++ backend/test.py
@@ -26,25 +26,44 @@ import sys
 #             price -= 1
 
 
-day = int(input())
+# day = int(input())
+#
+# curr_day = 1
+# price = 0
+# res = 0
+# up_limit = 0
+# up_count = 0
+#
+# while curr_day <= day:
+#     if up_count <= up_limit:
+#         up_count += 1
+#         price += 1
+#         res = price
+#         curr_day += 1
+#     else: # up_count == up_limit
+#         up_limit += 1
+#         up_count = 0
+#         price -= 1
+#         res = price
+#         curr_day += 1
+#
+# sys.stdout.write(str(res))
 
-curr_day = 1
-price = 0
-res = 0
-up_limit = 0
-up_count = 0
+import time
+import random
+import requests
+import datetime
 
-while curr_day <= day:
-    if up_count <= up_limit:
-        up_count += 1
-        price += 1
-        res = price
-        curr_day += 1
-    else: # up_count == up_limit
-        up_limit += 1
-        up_count = 0
-        price -= 1
-        res = price
-        curr_day += 1
 
-sys.stdout.write(str(res))
+def test():
+    while True:
+        time.sleep(1)
+        tick = time.time()
+        requests.get('http://127.0.0.1:8000/api/v1.0/service/test')
+        requests.get('http://127.0.0.1:8000/api/v1.0/service/image/list')
+        tock = time.time()
+        print('send request at %s, request cost: %.4f' % (str(datetime.datetime.now()), tock - tick))
+
+
+if __name__ == '__main__':
+    test()
